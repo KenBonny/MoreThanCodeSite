d0a0eca1d8bf729a8dd9aefa7a4e1ea71717090f
Ken Bonny <bonny.ken+github@gmail.com>
2019-08-15 14:07:44 +0200

    add publish script

---
+++ Publish.ps1
@@ -0,0 +1,9 @@
+hugo -d C:\temp\MoreThanCodeSite
+git checkout master
+Remove-Item .\*
+Move-Item C:\temp\MoreThanCodeSite\* .
+git add .
+## git cm 'Update site'
+## git pull
+## git push
+##git checkout redesign-hugo
