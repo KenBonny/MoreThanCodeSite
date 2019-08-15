1aeda2c5121e11ca21072e1532162d48fd4b6bf4
Ken Bonny <bonny.ken+github@gmail.com>
2019-08-15 14:35:24 +0200

    fine tune publish script

--- Publish.ps1
+++ Publish.ps1
@@ -1,8 +1,8 @@
 hugo -d C:\temp\MoreThanCodeSite
 git checkout master
-Remove-Item .\*
-Move-Item C:\temp\MoreThanCodeSite\* .
-git add .
+##Remove-Item .\*
+##Move-Item C:\temp\MoreThanCodeSite\* .
+##git add .
 ## git cm 'Update site'
 ## git pull
 ## git push
