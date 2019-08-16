2d7cfdd4fa8ccf34cddfdda2f764750a64a9cec6
Ken Bonny <bonny.ken+github@gmail.com>
2019-08-16 14:35:02 +0200

    stage all changes

--- Publish.ps1
+++ Publish.ps1
@@ -3,7 +3,7 @@ git checkout master
 Get-ChildItem . -Exclude .\.git\* | Remove-Item -Force -Recurse
 Copy-Item C:\temp\MoreThanCodeSite\* . -Force -Recurse
 Remove-Item C:\temp\MoreThanCodeSite -Force -Recurse
-##git add .
+git add .
 ## git cm 'Update site'
 ## git pull
 ## git push
