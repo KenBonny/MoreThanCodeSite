3d9e04b565e0ace7f04c11f67d732f582bc0ea2d
Ken Bonny <bonny.ken+github@gmail.com>
2019-08-16 14:38:08 +0200

    commit changes

--- Publish.ps1
+++ Publish.ps1
@@ -4,7 +4,7 @@ Get-ChildItem . -Exclude .\.git\* | Remove-Item -Force -Recurse
 Copy-Item C:\temp\MoreThanCodeSite\* . -Force -Recurse
 Remove-Item C:\temp\MoreThanCodeSite -Force -Recurse
 git add .
-## git cm 'Update site'
+git cm 'Update site'
 ## git pull
 ## git push
 ##git checkout redesign-hugo
